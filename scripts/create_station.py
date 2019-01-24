4f16b7c5e28baf6fdf6739247a44cc20f784ca57
maxime <maxime.pillon@epitech.eu>
2019-01-24 12:46:02 +0100

    adding script wicth normally should work

---
+++ scripts/create_station.py
@@ -0,0 +1,22 @@
+import sys
+import psycopg2
+
+conn = None
+try:
+    conn = psycopg2.connect("dbname = '' user = '' host = '' password = ''")
+except psycopg2.DatabaseError as ex:
+    print("I am unable to connect the database: {0}".format(ex))
+    sys.exit(1)
+
+curs = conn.cursor()
+curs.execute("""
+        INSERT INTO station (station_id, city, country, postal_code, xlocation, ylocation)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """, (1, 'Zagreb', 'Croatia', '10000', '45.814', '15.945'))
+
+# Maybe we will have to replace true with 1 don't know yet
+# duplicate this line has many time has needed for each sensor
+curs.execute("""
+        INSERT INTO sensor (sensor_id, voltage, is_activated, data_type, name, station_id)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """, (1, '12', 'true', 'mm', 'Precipitation', '1'))
---
+++ scripts/data_push.py
@@ -0,0 +1,44 @@
+import sys
+import psycopg2
+import time
+import datetime
+
+
+conn = None
+try:
+    conn = psycopg2.connect("dbname = '' user = '' host = '' password = ''")
+except psycopg2.DatabaseError as ex:
+    print("I am unable to connect the database: {0}".format(ex))
+    sys.exit(1)
+
+curs = conn.cursor()
+
+# need to put name send by the board with the corresponding id created with create_station.py
+sensors = {
+    'Precipitation': 1,
+    'Humidity': 2,
+    'Temperature': 3
+}
+
+
+# do what it need to recieve data as tuple
+def recieve_data():
+    return {}
+
+
+def data_pushing(infos):
+    date = datetime.datetime.now()
+    for key, val in infos.items():
+        curs.execute("""
+            INSERT INTO data (value, created_at, sensor_id)
+            VALUES (%(info)s, %(date)s, %(id)s);
+            """, {'info': val, 'date': date, 'id': sensors[key]})
+
+
+if __name__ == '__main__':
+    while 1:
+        data = recieve_data()
+        if data:
+            data_pushing(data)
+            data = []
+        time.sleep(1)
